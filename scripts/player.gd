7b795b413a1192e6b8fed406bb00ff4ba16121eb
jrinky <jrinkyx@gmail.com>
2025-11-14 17:02:48 -0500

    started test game project

---
+++ scripts/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+const SPEED := 200.0
+
+func _physics_process(delta: float) -> void:
+    var input_vector := Vector2.ZERO
+    input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+    input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+    if input_vector.length() > 0.0:
+        input_vector = input_vector.normalized()
+
+    velocity = input_vector * SPEED
+    move_and_slide()
