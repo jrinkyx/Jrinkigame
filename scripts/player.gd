d49b39c73e3c98ddcc956d88229e0f31f2e35d39
jrinky <jrinkyx@gmail.com>
2025-11-15 15:18:19 -0500

    fixed an error

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED := 200.0
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
     var input_vector := Vector2.ZERO
     input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
     input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
